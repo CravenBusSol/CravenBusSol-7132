ff314d2d0b9e6f9dbd7e59c526aa86ccb3510acd
shinchan3102 <ankitkumardos47@gmail.com>
2026-05-06 11:28:14 +0530

    fix cors error in auth api

--- packages/web/src/api/index.ts
+++ packages/web/src/api/index.ts
@@ -3,7 +3,7 @@ import { cors } from "hono/cors"
 
 const app = new Hono()
   .basePath('api')
-  .use(cors({ origin: "*" }))
+  .use(cors({ origin: (origin) => origin ?? "*", credentials: true }))
   .get('/ping', (c) => c.json({ message: `Pong! ${Date.now()}` }))
   .get('/health', (c) => c.json({ status: 'ok' }));
 
